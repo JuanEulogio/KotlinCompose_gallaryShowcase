a64a3dfd48967137207c00d089cd6faab9fa1207
juaneulogio <jceulogio243@gmail.com>
2024-01-22 20:16:01 -0600

    update: Imporoved UI. - Added weigth so we can select how to distribute space in our UI. - Horizonal View friendly - scrollable

--- app/src/main/java/com/cs407/kotlincompose_gallaryshowcase/MainActivity.kt
+++ app/src/main/java/com/cs407/kotlincompose_gallaryshowcase/MainActivity.kt
@@ -2,24 +2,19 @@ package com.cs407.kotlincompose_gallaryshowcase
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,22 +26,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.integerArrayResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.drawable.toDrawable
 import com.cs407.kotlincompose_gallaryshowcase.ui.theme.KotlinCompose_gallaryShowcaseTheme
-import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
 
@@ -93,25 +82,28 @@ fun gallaryLayout(modifier: Modifier = Modifier) {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(20.dp)
+        //TODO: didnt use center because when i had verticalScroll on, everything centered,
+        // as expected but that means we didnt have the extra space given as before (for some reason)
+        // AND because
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.padding(20.dp).verticalScroll(rememberScrollState())
     ) {
+        //TODO: Note how we put spacer
         Spacer(modifier = modifier.height(80.dp))
 
         artPicture(
             art = gallaryDrawingID[artNumber],
             artDesc = stringArrayResource(id = R.array.artName)[artNumber],
-            modifier = modifier.padding(8.dp)
+            //TODO: note we will use weight to distribute space selectively
+            modifier = modifier.weight(0.8f)
         )
-
-        //TODO: Note how we put spacer
-        Spacer(modifier = modifier.height(40.dp))
 
 
         artTitleArtist(
             title = stringArrayResource(id = R.array.artName)[artNumber],
             artist = stringArrayResource(id = R.array.artAuthor)[artNumber],
             year= stringArrayResource(id = R.array.artYear)[artNumber],
+            modifier = modifier.weight(0.1f)
         )
 
         //TODO: NOTE how we pass our function we want here
@@ -126,8 +118,14 @@ fun gallaryLayout(modifier: Modifier = Modifier) {
                     artNumber=gallaryMaxSize
                 }else{ artNumber--}
             },
-            modifier.padding(8.dp)
-            )
+            //TODO: passing over fillMaxSize kept our buttons when we turned horizontally.
+            // for some reason they didnt show up otherwise. If you mess with the modifiers,
+            // i believe it has something to do with it.
+            // if the parameter modifier is used in row(), it will remove buttons from our
+            // horizontal view, unless our other buttons are also the same modifier
+            //In short, just pass the attributes we want our child composable to have
+            modifier= modifier.weight(0.1f).fillMaxSize()
+        )
     }
 }
 
@@ -141,7 +139,8 @@ fun artPicture(modifier: Modifier = Modifier,
         Image(
             painter = painterResource(id = art),
             contentDescription = artDesc,
-            modifier = modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            contentScale = ContentScale.FillWidth
         )
     }
 }
@@ -154,7 +153,7 @@ fun artTitleArtist(modifier: Modifier = Modifier,
 
     Surface(
         color = colorResource(id = R.color.lightGray),
-        modifier = modifier.padding(16.dp)
+        modifier = Modifier.padding(16.dp)
     ) {
         Column {
             Text(
@@ -162,21 +161,21 @@ fun artTitleArtist(modifier: Modifier = Modifier,
                 fontFamily = FontFamily.SansSerif,
                 fontWeight = FontWeight.Light,
                 fontSize = 24.sp,
-                modifier = modifier.padding(start = 12.dp, top= 12.dp, end = 12.dp)
+                modifier = Modifier.padding(start = 12.dp, top= 12.dp, end = 12.dp)
             )
             Row {
                 Text(
                     text = artist,
                     fontWeight = FontWeight.Bold,
                     fontSize = 12.sp,
-                    modifier = modifier.padding(start = 12.dp, bottom = 12.dp)
+                    modifier = Modifier.padding(start = 12.dp, bottom = 12.dp)
                 )
                 Text(
                     text = year,
                     fontFamily = FontFamily.SansSerif,
                     fontWeight = FontWeight.Light,
                     fontSize = 12.sp,
-                    modifier = modifier.padding(end= 12.dp, bottom = 12.dp)
+                    modifier = Modifier.padding(end= 12.dp, bottom = 12.dp)
                 )
             }
         }
@@ -191,12 +190,12 @@ fun navitationButtons(
     onPreviousButtonClick: () -> Unit,
     modifier: Modifier = Modifier) {
     Row(
-        modifier = modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
         //TODO: NOTE space between will bring our buttons to end of bottomStart and bottom
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Button(
-            modifier = modifier.align(alignment = Alignment.Bottom),
+            modifier = Modifier.align(alignment = Alignment.Bottom),
             onClick = {
                 //TODO: Note how we pass in a function
                 onPreviousButtonClick()
@@ -205,7 +204,7 @@ fun navitationButtons(
         }
 
         Button(
-            modifier = modifier.align(alignment = Alignment.Bottom),
+            modifier = Modifier.align(alignment = Alignment.Bottom),
             onClick = {
                 onNextButtonClick()
             }) {
